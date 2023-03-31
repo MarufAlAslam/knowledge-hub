52361f9e75a0abd05a7b0e50e0156cf8a3c3233b
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-03-31 23:52:55 +0600

    fake data loaded

--- src/components/body/Blogs.jsx
+++ src/components/body/Blogs.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { BsBookmarkCheck } from 'react-icons/bs'
 
-const Blogs = () => {
+const Blogs = ({blogs}) => {
+    // console.log(blogs)
     return (
-        <div>
-            Blogs
+        <div className='lg:pr-10 pr-0'>
+            {
+                blogs.map(blog => (
+                    <div key={blog.id} className="blog-container mb-5">
+                        <img src={blog.image} className='w-full h-[300px] object-cover rounded-md mb-4' alt="" />
+                        <div className="flex justify-between items-center">
+                        <div className="flex items-center justify-start">
+                            <img src={blog.avatar} className='w-10 h-10 rounded-full mr-4' alt="" />
+                            <div className="flex flex-col">
+                                <h1 className='text-lg font-bold'>{blog.name}</h1>
+                                <p className='text-sm text-gray-500'>{blog.postedOn}</p>
+                            </div>
+                        </div>
+                        <div className="bookmark flex justify-end items-center">
+                            <p>
+                                {blog.minutesToRead < 10 ? `0${blog.minutesToRead}` : blog.minutesToRead} min read
+                            </p>
+                            <button className='btn ml-2'>
+                                <BsBookmarkCheck/>
+                            </button>
+                        </div>
+                        </div>
+
+                        <h1 className='text-2xl font-bold mt-4'>{blog.title}</h1>
+                        <p>
+                        {
+                            blog.tags.map(tag => (
+                                <button key={tag} className='btn btn-sm btn-outline-primary ml-0 mr-3 text-gray-500 mt-2'>#{tag}</button>
+                            ))
+                        }
+                        </p>
+                        <button className='btn font-semibold underline mt-3 text-[#6047EC]'>
+                            Mark as read
+                        </button>
+                        <hr className='mt-5' />
+                    </div> ))
+            }
         </div>
     )
 }
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Blogs from './Blogs'
 import Sidebar from './Sidebar'
 
+
 const Body = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        fetch("./data.json")
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+            .catch(err => console.log(err))
+    }, [])
+
+    // console.log(blogs)
+
     return (
         <div>
             <hr className='my-4' />
             <div className="lg:flex md:block mt-4">
                 <div className="blogs-container lg:w-3/4 w-full">
-                    <Blogs/>
+                    <Blogs blogs={blogs}/>
                 </div>
                 <div className="sidebar-container lg:w-1/4 w-full">
                     <Sidebar/>
